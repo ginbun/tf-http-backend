805a3716bacbbad2a695a00cb40219ef4b3f4daf
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2023-12-28 21:36:54 +0000

    First finished beta version

--- app.py
+++ app.py
@@ -1,16 +1,175 @@
-#!python3
+from argparse import ArgumentParser
+from configparser import ConfigParser
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import HTTPException, Locked
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import logging
 
-from flask import Flask
+### LOGGING ###
+# Format and Get root logger
+LOG_LEVEL = logging.DEBUG
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)-8s][%(name)-16s][%(lineno)d] %(message)s',
+    level=LOG_LEVEL,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger()
 
+### ARGUMENTS ###
+parser = ArgumentParser()
+parser.add_argument("-c","--config-file", type=str, dest="config_file", default="./config.ini", help="Path to config file.")
+args = parser.parse_args()
+
+### GENERAL FUNCTIONS ###
+def load_config_file(path: str):
+    config = ConfigParser()
+    config.read(
+        filenames = path
+    )
+    return config
+
+### APP DEFINITION ###
 app = Flask(__name__)
 
-@app.route("/")
-def Welcome():
-  return "This is my first web app! Yay!"
+### DATABASE ###
+# Read config file
+config = load_config_file(path=args.config_file)
+# Connect to DB
+# logger.debug(f"mysql+mysqlconnector://{config['database']['user']}:{config['database']['pswd']}@{config['database']['host']}:{config['database']['port']}/{config['database']['schema']}")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://{config['database']['user']}:{config['database']['pswd']}@{config['database']['host']}:{config['database']['port']}/{config['database']['schema']}"
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
-@app.route("/potato")
-def potato():
-  return "This is a potato app!"
+db = SQLAlchemy(app=app)
 
 
-app.run()
+class TFState(db.Model):
+    __tablename__ = 'tfstates'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), unique = True, nullable = False)
+    contents = db.Column(db.JSON, nullable = False)
+    created_at = db.Column(db.TIMESTAMP, default = datetime.now(), nullable = False)
+    last_changed = db.Column(db.TIMESTAMP, nullable = False)
+
+    def __repr__(self) -> str:
+        return f"{self.name - self.contents}"
+
+class LockedState(db.Model):
+    __tablename__ = 'locked_tfstates'
+    name = db.Column(db.String(50), primary_key = True)
+
+    def __repr__(self) -> str:
+        return (self.name)
+    
+### EXCEPTIONS ###
+# class StateLockedException(HTTPException):
+#     title = "Locked TFState"
+#     code = 423
+#     description = 'TFState is in locked state.'
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return e
+    # now you're handling non-HTTP exceptions only
+    res = {'code': 500,
+           'errorType': 'Internal Server Error',
+           'errorMessage': "Something went really wrong!"}
+    res['errorMessage'] = e.message if hasattr(e, 'message') else f'{e}'
+    return jsonify(res), 500
+# app.register_error_handler(StateLockedException, handle_exception)
+
+
+### API ###
+# Set base path
+# app.config["APPLICATION_ROOT"] = "/tf-http/api/v1"
+
+
+### API Paths ###
+@app.route("/tfstates/all", methods=["GET"])
+def get_all_states():
+    logger.debug("Querying states")
+    states = db.session.query(TFState).all()
+    logger.debug("Building output")
+    output = []
+    logger.debug("Filling Output list")
+    for state in states:
+        output.append({"id": state.id, "name": state.name, "contents": state.contents, "created_at": state.created_at, "last_changed": state.last_changed})
+    logger.debug("Returning output and 200 http code")
+    return (output,200)
+
+@app.route("/tfstates/<name>", methods=["GET"])
+def get_tfstate(name):
+    logger.debug("Building query")
+    query = db.session.query(TFState).filter_by(name = name)
+    logger.debug("Executing query")
+    state = query.one_or_none()
+    logger.debug("Checking results")
+    if state is not None:
+        return state.contents
+    else:
+        return ("Not Found",404)
+
+@app.route("/tfstates/<name>", methods=["POST"])
+def update_tfstate(name):
+    query = db.session.query(TFState).filter_by(name = name)
+    state = query.one_or_none()
+    if state is None:
+        logger.debug("Creating TFState ojbect")
+        state = TFState()
+        state.name         = name
+        state.contents     = request.json
+        state.created_at   = datetime.now()
+        state.last_changed = datetime.now()
+        logger.debug(f"TFState object created: \n{state.contents}")
+        db.session.add(state)
+        logger.debug(f"Object added to database")
+        db.session.commit()
+        logger.debug(f"Transaction commited to DB")
+        http_code = 201
+    else:
+        logger.debug(f"Got TFState ojbect from DB - ID: {state.id}")
+        state.contents = request.json
+        logger.debug("Updated contents of TFState")
+        state.last_changed = datetime.now()
+        logger.debug("Updated last_changed of TFState")
+        db.session.commit()
+        http_code = 200
+    return ("OK", http_code)
+
+@app.route("/tfstates/<name>", methods=["LOCK"])
+def lock_tfstate(name):
+    logger.debug("Trying to find the state first")
+    query = db.session.query(TFState).filter_by(name = name)
+    state = query.one_or_none()
+    if state is None:
+        return ("Non-existent state", 404)
+    else:
+        locked_state = db.session.query(LockedState).get(name)
+        if locked_state is None:
+            locked_state = LockedState()
+            locked_state.name = name
+            db.session.add(locked_state)
+            db.session.commit()
+            return ("State locked", 200)
+        else:
+            return ("State already locked", 423)
+
+@app.route("/tfstates/<name>", methods=["UNLOCK"])
+def unlock_tfstate(name):    
+    locked_state = db.session.query(LockedState).get(name)
+    if locked_state is not None:
+        db.session.delete(locked_state)
+        db.session.commit()
+        return ("State unlocked", 200)
+    else:
+        return ("State already unlocked", 409)
+
+### MAIN ###
+if __name__ == "__main__":
+    app.run(
+        host = config['flask']['address'],
+        port = config['flask']['port'],
+        debug = config['flask']['debug']
+    )
